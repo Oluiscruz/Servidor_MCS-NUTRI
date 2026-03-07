129e2b345c71a40d4fe724f48224cdf0c043e819
Luis Alberto <oluiscruz25@gmail.com>
2026-03-07 13:53:22 -0300

    fix email timeout

--- migrations/005_fiz_table_agendamentos.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE agendamentos 
-    RENAME COLUMN mes_ano TO mes;
-
-ALTER TABLE agendamentos 
-    ALTER COLUMN mes TYPE meses_ano USING mes::text::meses_ano;
-
--- Remove o tipo antigo
-DROP TYPE meses_ano_old;
--- routes/routes.js
+++ routes/routes.js
@@ -13,11 +13,19 @@ const routes = express.Router();
 
 // Configurar transporter de email
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-    }
+    },
+    tls: {
+        rejectUnauthorized: false
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 const uploadsDir = path.resolve(__dirname, '..', 'crn_documento');
