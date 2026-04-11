acc8096d6542c40df37440e93e8a437cefdbed6d
Luis Alberto <oluiscruz25@gmail.com>
2026-04-10 23:22:26 -0300

    fix error on bd

--- migrations/005_rename_mes_ano_to_mes.sql
+++
@@ -1,6 +0,0 @@
--- Migration: 005_rename_mes_ano_to_mes
--- Description: Renomeia coluna mes_ano para mes na tabela agendamentos
--- Date: 2026-03-07
-
-ALTER TABLE agendamentos 
-    RENAME COLUMN mes_ano TO mes;
